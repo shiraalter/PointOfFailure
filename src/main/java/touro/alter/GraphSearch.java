dd7d154f496ac563b931326095a0b4d285d6bd3a
Mindy Gottlieb <mindy@mindys-mbp.myfiosgateway.com>
2020-11-16 09:55:43 -0500

    added comments, cleaned up, changed implementation a bit based on the other classes

--- src/main/java/touro/alter/GraphSearch.java
+++ src/main/java/touro/alter/GraphSearch.java
@@ -4,18 +4,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class GraphSearch {
-    private final ArrayList<GraphNode> nodes;
+    private ArrayList<GraphNode> nodes = new ArrayList<>();
     private ArrayList<GraphNode> tempNodes = new ArrayList<>();
     private ArrayList<GraphNode> remainingConnections = new ArrayList<>();
     private HashMap<GraphNode, Integer> pointsOfFailure = new HashMap<>();
     private GraphNode potentialSPF;
     private int numSubnets;
 
-    public GraphSearch(ArrayList<GraphNode> nodes) {
-        this.nodes = nodes;
-    }
-
-    public HashMap<GraphNode, Integer> getPointsOfFailure() {
+    public HashMap<GraphNode, Integer> getPointsOfFailure(ArrayList<GraphNode> nodesList) {
+        nodes.clear();
+        nodes = nodesList;
         for (GraphNode node : nodes) {
             potentialSPF = node;
             numSubnets = 0;
@@ -28,6 +26,7 @@ public class GraphSearch {
         return pointsOfFailure;
     }
 
+    //remove node by copying the list into a different array, without that one node
     private void removeNode(GraphNode nodeToRemove) {
         tempNodes.clear();
         for (GraphNode node : nodes) {
@@ -38,37 +37,38 @@ public class GraphSearch {
         }
     }
 
+    //search for the SPF starting with the first node
     private void findSPF(GraphNode node) {
         remainingConnections.clear();
 
         GraphNode startingNode = tempNodes.get(0);
         search(startingNode, node);
 
+        //if the two are not equal in size, some nodes could not be reached - there is an SPF
         if (remainingConnections.size() != tempNodes.size()) {
             numSubnets++;
             for (GraphNode connectedNode : remainingConnections) {
                 tempNodes.remove(connectedNode);
             }
+            //now use the remaining nodes in temp nodes to find how many more subnets there are
             findSPF(node);
         }
 
+        //at the last subnet, remainingConnections and tempNodes will be the same size.
+        //it is a subnet then, if tempNodes is not the same size as nodes, without the one we removed
         else if (tempNodes.size() != nodes.size() - 1){
             numSubnets++;
         }
 
     }
 
+    //recursive search to find the SPF, if any
     private void search(GraphNode startingNode, GraphNode nodeRemoved) {
-//        TODO: this prbly has a bug because it doesnt check nodes past the first connection it finds
         remainingConnections.add(startingNode);
-        int index = 0;
         for (GraphNode child : startingNode.getConnections()) {
-            index++;
             if (!child.equals(nodeRemoved) && !remainingConnections.contains(child)) {
                 search(child, nodeRemoved);
             }
-        }
-        if (index == startingNode.getConnections().size()) {
         }
     }
 }
