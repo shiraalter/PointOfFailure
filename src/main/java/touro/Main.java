f99e05c7b7b062885662a250e698c1e29a5e72df
Jennifer Komendant <jennifer.komendant@gmail.com>
2020-11-16 22:18:30 -0500

    added controller to main

--- src/main/java/touro/Main.java
+++ src/main/java/touro/Main.java
@@ -1,16 +1,20 @@
 package touro;
 
 
+import java.util.List;
+
 public class Main {
 
 
     public static void main(String[] args) {
         GraphInput creator = new GraphInput(args[0]);
         GraphSearch search = new GraphSearch();
+        List<List<GraphNode>> netList = creator.getNetList();
         GraphView view = new GraphView(search, creator);
         GraphFrame frame = new GraphFrame(view);
 
         frame.setVisible(true);
+        GraphController graphController = new GraphController(netList, search);
 
     }
 
